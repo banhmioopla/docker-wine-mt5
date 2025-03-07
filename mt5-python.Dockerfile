4133b513777baae614eca1dd368388c08e25854a
banhmioopla <qbingking@gmail.com>
2025-03-07 17:46:36 +0700

    Fix

--- mt5-python.Dockerfile
+++ mt5-python.Dockerfile
@@ -1,19 +1,13 @@
-# Sử dụng Python 3.11 làm base image
 FROM python:3.10
 
-# Đặt thư mục làm việc
 WORKDIR /app
 
-# Copy file requirements.txt trước để cache dependencies
 COPY requirements.txt /app/
 
-# Cài đặt các thư viện Python, bao gồm mt5linux
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy toàn bộ thư mục script vào container
 COPY script /app/script
 
-# Mở cổng FastAPI
 EXPOSE 8000
 
 # Lệnh khởi động FastAPI server
--- script/test.py
+++ script/test.py
@@ -1,15 +1,24 @@
-from mt5linux import MetaTrader5
+import sys
 import os
+import rpyc
+import time
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
-# connecto to the server
-mt5 = MetaTrader5(
-    host = os.getenv('MT5_HOST'),
-    port = os.getenv('MT5_PORT')
-) 
-# use as you learned from: https://www.mql5.com/en/docs/integration/python_metatrader5/
-mt5.initialize()
-mt5.terminal_info()
-mt5.copy_rates_from_pos('VALE3',mt5.TIMEFRAME_M1,0,1000)
-# ...
-# don't forget to shutdown
-mt5.shutdown()
+
+conn = rpyc.classic.connect(host="192.168.65.0/24", port=5900)
+print(conn)
+time.sleep(5)
+
+def login_remote(account_id, pw, server):
+    print("Ok Remote")
+    import MetaTrader5 as _mt5
+    _mt5.initialize(login = account_id, password = pw, server = server)
+    return _mt5.account_info()._asdict()
+
+fn = conn.teleport(login_remote)
+time.sleep(5)
+
+data = fn(7300401,'1@NGeF3vvh7mIy','CMarketsSC-MT5-2')
+time.sleep(5)
+print('data', data)
+
